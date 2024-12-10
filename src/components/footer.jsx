0d67717bde6207ecf3e24ef7a466b92f2949d75a
terrencethe1 <terrence.jsr@gmail.com>
2024-12-10 09:53:54 -0600

    created footer.jsx and added icons to social links

---
+++ src/components/footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaFacebookF, FaTwitter, FaInstagram, FaGithub, FaLinkedin  } from "react-icons/fa";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="social-icons">
+        <a href="https://github.com/terrencethe1" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/terrence-johnson-3033b3341" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+          <FaLinkedin />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+          <FaInstagram />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
