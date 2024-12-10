23e21d40a4a836c3969116d48981f33d914cfc97
terrencethe1 <terrence.jsr@gmail.com>
2024-12-09 23:13:38 -0600

    Created about.jsx file

---
+++ src/components/about.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About me</h1>
+      <p>
+        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch
+        Etsy authentic Marfa quinoa typewriter plaid direct trade small batch
+        wayfarers...
+      </p>
+    </div>
+  );
+};
+
+export default About;
